ba3803c4296cb92b384a908fefba51da6d0ec4bb
ivanpolanis <polanisivan@gmail.com>
2023-04-07 11:50:56 -0300

    Practica 4B - Ejercicio 1

---
+++ practica4/b/ArbolBinarioTest.java
@@ -0,0 +1,26 @@
+package practica4.b;
+
+import classes.ArbolBinario;
+import utils.Utils;
+
+public class ArbolBinarioTest {
+
+	public static void main(String[] args) {
+		ArbolBinario<Integer> arbolBinarioA;
+		// ----- arbolBinarioA -----
+		arbolBinarioA = new ArbolBinario<Integer>(1);
+		ArbolBinario<Integer> hijoIzquierdo = new ArbolBinario<Integer>(2);
+		hijoIzquierdo.agregarHijoIzquierdo(new ArbolBinario<Integer>(3));
+		hijoIzquierdo.agregarHijoDerecho(new ArbolBinario<Integer>(4));
+		ArbolBinario<Integer> hijoDerecho = new ArbolBinario<Integer>(6);
+		hijoDerecho.agregarHijoIzquierdo(new ArbolBinario<Integer>(7));
+		hijoDerecho.agregarHijoDerecho(new ArbolBinario<Integer>(8));
+		arbolBinarioA.agregarHijoIzquierdo(hijoIzquierdo);
+		arbolBinarioA.agregarHijoDerecho(hijoDerecho);
+
+
+		System.out.println(Utils.sumaMaximaVertical(arbolBinarioA));
+
+	}
+
+}
--- utils/Utils.java
+++ utils/Utils.java
@@ -4,6 +4,19 @@ import classes.ArbolBinario;
 
 public class Utils {
   public static int sumaMaximaVertical(ArbolBinario<Integer> arbol) {
-    return 1;
+    // Si es hoja devolvemos el dato
+    if (arbol.esHoja())
+      return arbol.getDato();
+    int der = 0;
+    int izq = 0;
+    // Si no es hoja, sumamos el dato con la suma maxima de los hijos
+    if (arbol.tieneHijoIzquierdo()) {
+      izq += sumaMaximaVertical(arbol.getHijoIzquierdo()) + arbol.getDato();
+    }
+    if (arbol.tieneHijoDerecho()) {
+      der += sumaMaximaVertical(arbol.getHijoDerecho()) + arbol.getDato();
+    }
+    // Devolvemos la suma maxima
+    return Math.max(der, izq);
   }
 }
